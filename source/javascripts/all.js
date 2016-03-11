f6492f249a2b5fdf3d27dc4948211cc5854ee72f
xavipzc <x.pouzenc@pzc.fr>
2016-03-11 15:52:32 +0100

    adding animations
    add animations on the home page

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,34 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function(){
+
+	$(window).on("scroll", function(){
+		if ( $(this).scrollTop() > 200 ){
+			$(".navbar").css("background-color", "rgba(0, 0, 0, 0.8)")
+		}
+
+		else {
+			$(".navbar").css("background-color", "transparent")
+		}
+	});
+
+	$('a[href*="#"]:not([href="#"])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html, body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+
+
+
+
+
+});
